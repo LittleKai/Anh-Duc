108eb0407be49b28a98962801d73072afc25a6dd
Anh Duc <aduc5525@gmail.com>
2016-07-22 10:57:42 +0700

    1

---
+++ Test/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+
+public class Main {
+
+public static void main(String[] args) {
+	HashMap<String, Double> product = new HashMap<>();
+	product.put("A10", 200.6);
+	product.put("A01", 100.0);
+	product.put("B05", 150.5);
+	product.put("D05", 250.0);
+	String id = "";
+	try
+	{
+		getProduct(id);
+	System.out.println(product.get("A10"));
+		}
+	catch(NoSuchProductReception e){
+		System.out.println(e.getMessage());
+		}
+
+	}
+
+private static void getProduct(String id)throws NoSuchProductReception {
+	// TODO Auto-generated method stub
+	if (id.equals(""))
+		throw new NoSuchProductReception("Khong co san phan nhu vay.");
+
+	
+}
+
+}
+
---
+++ Test/src/NoSuchProductReception.java
@@ -0,0 +1,12 @@
+
+public class NoSuchProductReception extends Exception {
+	public NoSuchProductReception(String message)
+	{
+	super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+}
